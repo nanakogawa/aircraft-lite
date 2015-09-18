a1c07059bcc63b135e71383fecaabfb6c0968015
nana kogawa <kogawanana@gmail.com>
2015-09-18 12:40:47 -0700

    issue #1 finished form skeleton with temporary layout and colors.

--- js/server.js
+++ js/server.js
@@ -10,7 +10,7 @@
  app.use(express.static(__dirname + '/../css'));
  app.use(express.static(__dirname + '/../fonts'));
  app.use(express.static(__dirname + '/../images'));
- app.use('/api', require('./routes/api.js'));
+ app.use('/api', require('./api.js'));
 
  app.get('/', function(req, res) {
  	res.sendFile(path.join(__dirname + '/../html/index.html'));
