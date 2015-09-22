49a534039f6b5a5f49c2252ea8053f51cbc96fb2
nana kogawa <kogawanana@gmail.com>
2015-09-22 11:22:19 -0700

    added google map #4

---
+++ js/map.js
@@ -0,0 +1,17 @@
+function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 4
+  });
+
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng,
+    title: 'Hello World!'
+  });
+}
